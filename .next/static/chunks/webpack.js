46c303308cb0a0aa269b2b97652cb849b4355406
ragnamarie <ragna.steinboemer@gmail.com>
2024-04-17 14:26:07 +0200

    different login

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "87ca41d6fde84b39"; }
+/******/ 		__webpack_require__.h = function() { return "134a8cb0f5caf36e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -42,15 +42,48 @@ import clientPromise from "@/db/mongodb";
 // change to this in order to allow login for everyone so that new user can be created.
 // alternatively I can create a new user manually in the database
 
+// export default NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+
+//   secret: process.env.JWT_SECRET,
+//   adapter: MongoDBAdapter(clientPromise),
+
+//   callbacks: {
+//     async session({ session, user }) {
+//       if (session?.user) {
+//         session.user.id = user.id;
+//         session.user.googleId = user.googleId;
+//         session.user.email = user.email;
+//         session.user.admin = user.admin;
+//       }
+//       return session;
+//     },
+//   },
+// });
+
 export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.sub, // 'sub' is the unique identifier in Google profile
+          email: profile.email,
+          name: profile.name,
+          userName: profile.email.split("@")[0], // Using the email as a username (modify as needed)
+          googleId: profile.sub, // Assuming Google provides a unique ID
+          admin: false,
+        };
+      },
     }),
   ],
 
-  secret: process.env.JWT_SECRET,
   adapter: MongoDBAdapter(clientPromise),
 
   callbacks: {
@@ -64,4 +97,6 @@ export default NextAuth({
       return session;
     },
   },
+
+  secret: process.env.JWT_SECRET,
 });
