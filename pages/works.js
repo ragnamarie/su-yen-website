0dfb118a53a880748dc6849aae23b66a7520c9fa
ragnamarie <ragna.steinboemer@gmail.com>
2024-10-28 20:20:02 +0100

    test

--- pages/works.js
+++ pages/works.js
@@ -23,6 +23,10 @@ export default function WorksPage() {
       >
         {session && <ImageUploader />}
         <ImageList images={images} />
+        <img
+          src="https://www.su-yen.com/images/IMG_2941.jpg"
+          alt="Example Image"
+        />
       </div>
     </>
   );
